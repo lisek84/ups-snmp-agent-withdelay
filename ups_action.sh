7ef94fa6097915f2abaf519f465239d40f4a761e
lisek84 <klsoft@interia.pl>
2019-06-14 14:01:37 +0200

    Update ups_action.sh
    Found bug: grepping credentials file.

--- ups_action.sh
+++ ups_action.sh
@@ -28,14 +28,14 @@ if [ "$waituntildown" -eq 0 ]; then
 
                         echo "I'm just sending Shutdown procedure to every machine in group at once NOW!"
                         echo "SENDING SHUTDOWN TO MAIN MACHINE:"
-                        ipmilogin=`grep $MainMachineIP credentials.cfg | awk '{print $2}'`
-                        ipmipass=`grep $MainMachineIP credentials.cfg | awk '{print $3}'`
+                        ipmilogin=`grep -w $MainMachineIP credentials.cfg | awk '{print $2}'`
+                        ipmipass=`grep -w $MainMachineIP credentials.cfg | awk '{print $3}'`
                         $ipmitool -H $MainMachineIP -U $ipmilogin -P $ipmipass $ipmicommand
                                 echo "SHUTTING DOWN SLAVES NOW!"
                                 for slave in $SlavesIP; do
                                         echo Shutting down machine with $slave address.
-                                        ipmilogin=`grep $slave credentials.cfg | awk '{print $2}'`
-                                        ipmipass=`grep $slave credentials.cfg | awk '{print $3}'`
+                                        ipmilogin=`grep -w $slave credentials.cfg | awk '{print $2}'`
+                                        ipmipass=`grep -w $slave credentials.cfg | awk '{print $3}'`
                                         $ipmitool -H $slave -U $ipmilogin -P $ipmipass $ipmicommand
                                 done
                                 exit 0
@@ -45,8 +45,8 @@ if [ "$waituntildown" -eq 0 ]; then
 else
                         if [ "$killeitherway" -eq 0 ]; then echo "I'll wait and try to ping $MainMachineIP in $loopwaitdelay second loops. And I'll kill all the slaves: $SlavesIP but only if main server is really down and not responding. Else I'll end in endless loop of waiting"
                         echo "SENDING SHUTDOWN TO MAIN MACHINE:"
-                        ipmilogin=`grep $MainMachineIP credentials.cfg | awk '{print $2}'`
-                        ipmipass=`grep $MainMachineIP credentials.cfg | awk '{print $3}'`
+                        ipmilogin=`grep -w $MainMachineIP credentials.cfg | awk '{print $2}'`
+                        ipmipass=`grep -w $MainMachineIP credentials.cfg | awk '{print $3}'`
                         $ipmitool -H $MainMachineIP -U $ipmilogin -P $ipmipass $ipmicommand
                         echo "NOW. I WILL WAIT for $loopwaitdelay seconds and ping the machine in loop. If it will respond I will not do anything about this and just stuck in endless loop of waiting. If he'll not respond. I will Begin Shutting down Slave Machines"
                                 echo WAIT IS OVER. SENDING PING in $loopwaitdelay
@@ -62,8 +62,8 @@ else
                                 echo MAIN MACHINE IS DOWN. SHUTTING DOWN SLAVES NOW!
                                 for slave in $SlavesIP; do
                                         echo Shutting down machine with $slave address.
-                                        ipmilogin=`grep $slave credentials.cfg | awk '{print $2}'`
-                                        ipmipass=`grep $slave credentials.cfg | awk '{print $3}'`
+                                        ipmilogin=`grep -w $slave credentials.cfg | awk '{print $2}'`
+                                        ipmipass=`grep -w $slave credentials.cfg | awk '{print $3}'`
                                         $ipmitool -H $slave -U $ipmilogin -P $ipmipass $ipmicommand
                                 done
                                 exit 0
@@ -71,16 +71,16 @@ else
                         else
                         echo "I'll wait and try to ping $MainMachineIP for $wait_until_main_down_for seconds. And after this I'll kill all the slaves: $SlavesIP even is main server is still running."
                         echo "SENDING SHUTDOWN TO MAIN MACHINE:"
-                        ipmilogin=`grep $MainMachineIP credentials.cfg | awk '{print $2}'`
-                        ipmipass=`grep $MainMachineIP credentials.cfg | awk '{print $3}'`
+                        ipmilogin=`grep -w $MainMachineIP credentials.cfg | awk '{print $2}'`
+                        ipmipass=`grep -w $MainMachineIP credentials.cfg | awk '{print $3}'`
                         $ipmitool -H $MainMachineIP -U $ipmilogin -P $ipmipass $ipmicommand
                         echo "NOW. I WILL WAIT for $wait_until_main_down_for. Regardless of anything I will Begin Shutting down Slave Machines, even is Main Machine is still running. It was your choice in my configuration. Don't blame me is something nasty happens"
                                 sleep $wait_until_main_down_for
                                 echo "SHUTTING DOWN SLAVES NOW!"
                                 for slave in $SlavesIP; do
                                         echo Shutting down machine with $slave address.
-                                        ipmilogin=`grep $slave credentials.cfg | awk '{print $2}'`
-                                        ipmipass=`grep $slave credentials.cfg | awk '{print $3}'`
+                                        ipmilogin=`grep -w $slave credentials.cfg | awk '{print $2}'`
+                                        ipmipass=`grep -w $slave credentials.cfg | awk '{print $3}'`
                                         $ipmitool -H $slave -U $ipmilogin -P $ipmipass $ipmicommand
                                 done
                                 exit 0
